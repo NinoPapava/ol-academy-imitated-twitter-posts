219f5a8d5c8db8976b1c2151206a2e6ea7232324
NinoPapava <gv.papava@gmail.com>
2024-04-28 14:14:54 +0400

    (CRA) create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Post } from './components/Post';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Post />
+      </div>
+    );
+  }
+}
+
+export { App };
---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Post = () => {
+
+  return (
+    <div className='Post'>
+      Post
+    </div>
+  )
+}
+
+export { Post }
