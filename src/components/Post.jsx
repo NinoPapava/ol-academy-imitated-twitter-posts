04c2deacaa44ef37e2c85d65ded6c76de2a33a2a
NinoPapava <gv.papava@gmail.com>
2024-04-29 20:27:47 +0400

    Twitter ის პოსტის მარკაპი

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { Avatar } from '@material-ui/core'
+import { BarChart, BookmarkBorder, ChatBubbleOutline, Favorite, Repeat, VerifiedUser } from '@material-ui/icons'
+import '../Styles/posts.scss'
 
 const Post = () => {
 
   return (
-    <div className='Post'>
-      Post
-    </div>
+    <>
+      <div className='post'>
+        <div className='post__avatar'>
+          <Avatar />
+        </div>
+        <div className='post__body'>
+          <div className='post__header'>
+            <div className='post__headerText'>
+              <h3>post title</h3>
+              <span className='post__headerSp'>
+                <VerifiedUser className='post__badge' />
+                @ post userName
+              </span>
+              <div className='post__dropdown'>
+                <span>...</span>
+                <div className='post__dropdown'>
+                  <div className='post__dropdownContent'>
+                    <button>like</button>
+                    <button>See Tweet</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='post__bodySp'>
+              <p >post body</p>
+            </div>
+          </div>
+          <img src='' alt='' />
+          <div className='post__footer'>
+            <ChatBubbleOutline className='icon' />
+            <Repeat className='icon' />
+            <Favorite className='icon' />
+            <BarChart className='icon' />
+            <BookmarkBorder className='icon' />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
