cd12606db31d05466f0e9386376c7ea9318c012e
NinoPapava <gv.papava@gmail.com>
2024-04-29 20:41:01 +0400

    პოსტები წამოვიღეთ /posts დან

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,48 +1,58 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { Avatar } from '@material-ui/core'
 import { BarChart, BookmarkBorder, ChatBubbleOutline, Favorite, Repeat, VerifiedUser } from '@material-ui/icons'
 import '../Styles/posts.scss'
 
 const Post = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.error('Error fetching data:', error))
+  }, [])
 
   return (
     <>
-      <div className='post'>
-        <div className='post__avatar'>
-          <Avatar />
-        </div>
-        <div className='post__body'>
-          <div className='post__header'>
-            <div className='post__headerText'>
-              <h3>post title</h3>
-              <span className='post__headerSp'>
-                <VerifiedUser className='post__badge' />
-                @ post userName
-              </span>
-              <div className='post__dropdown'>
-                <span>...</span>
+      {posts.map(post => (
+        <div key={post.id} className='post'>
+          <div className='post__avatar'>
+            <Avatar />
+          </div>
+          <div className='post__body'>
+            <div className='post__header'>
+              <div className='post__headerText'>
+                <h3>{post.title}</h3>
+                <span className='post__headerSp'>
+                  <VerifiedUser className='post__badge' />
+                  @{post.userId}
+                </span>
                 <div className='post__dropdown'>
-                  <div className='post__dropdownContent'>
-                    <button>like</button>
-                    <button>See Tweet</button>
+                  <span>...</span>
+                  <div className='post__dropdown'>
+                    <div className='post__dropdownContent'>
+                      <button>like</button>
+                      <button>See Tweet</button>
+                    </div>
                   </div>
                 </div>
               </div>
+              <div className='post__bodySp'>
+                <p >{post.body}</p>
+              </div>
             </div>
-            <div className='post__bodySp'>
-              <p >post body</p>
+            <img src='' alt='' />
+            <div className='post__footer'>
+              <ChatBubbleOutline className='icon' />
+              <Repeat className='icon' />
+              <Favorite className='icon' />
+              <BarChart className='icon' />
+              <BookmarkBorder className='icon' />
             </div>
-          </div>
-          <img src='' alt='' />
-          <div className='post__footer'>
-            <ChatBubbleOutline className='icon' />
-            <Repeat className='icon' />
-            <Favorite className='icon' />
-            <BarChart className='icon' />
-            <BookmarkBorder className='icon' />
           </div>
         </div>
-      </div>
+      ))}
     </>
   )
 }
