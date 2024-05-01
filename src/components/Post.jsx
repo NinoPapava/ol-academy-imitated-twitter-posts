af22737b91b8eee25afb1161659aa747278a64d8
NinoPapava <gv.papava@gmail.com>
2024-05-01 16:18:07 +0400

    პოსტის პოპაპის გვერდის ლოგიკა, იმ შემთხვევაში თუ არჩეულია პოსტი და გახსნილია

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Avatar } from '@material-ui/core'
 import { BarChart, BookmarkBorder, ChatBubbleOutline, Favorite, Repeat, VerifiedUser } from '@material-ui/icons'
 import { Photos } from '../Api/Photos'
@@ -7,6 +7,15 @@ import '../Styles/posts.scss'
 
 const Post = () => {
   const [posts, setPosts] = useState([])
+  const [isPostOpen, setIsPostOpen] = useState(false)
+  const [selectedPost, setSelectedPost] = useState(null);
+  const postPopupRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (postPopupRef.current && postPopupRef.current.contains(event.target)) {
+      setIsPostOpen(false);
+    }
+  };
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -15,54 +24,72 @@ const Post = () => {
       .catch(error => console.error('Error fetching data:', error))
   }, [])
 
+  useEffect(() => {
+    isPostOpen
+      ? document.addEventListener('mousedown', handleClickOutside)
+      : document.removeEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isPostOpen]);
+
   return (
-    <div>
-    {posts.map(post => (
-      <div key={post.id} >
-        <div className='post'>
-          <div>
-            <div className='post__body'  >
-              <div className='post__avatar'>
-                <Avatar />
-              </div>
-              <h3>{post.title}</h3>
-              <span className='post__headerSp'>
-                <VerifiedUser className='post__badge' />
-                @{post.userId}
-              </span>
-            </div>
-            <div className='post__bodySp' >
-              <p >{post.body}</p>
-            </div>
-            <div className='post__image' >
-              <Photos postId={post.id} />
-            </div>
-          </div>
-          <div >
-            <div className='post__dropdown'>
-              <span className='post__span'>...</span>
-              <div className='post__dropdownSp'>
-                {/* აქ უნდა გავხსნა dropDown  */}
-                  <div className='post__dropdownContent'>
-                    <p>like</p>
-                    <p>See Tweet</p>
+    <>
+      {isPostOpen && selectedPost ? (
+        <>
+          {/* აქ იქნება დაკლიკული პოსტის პოპაპი */}
+        </>
+      ) : (
+        <>
+        {posts.map(post => (
+          <div key={post.id} >
+            <div className='post'>
+              <div>
+                <div className='post__body'  >
+                  <div className='post__avatar'>
+                    <Avatar />
                   </div>
-                  {/*  */}
+                  <h3>{post.title}</h3>
+                  <span className='post__headerSp'>
+                    <VerifiedUser className='post__badge' />
+                    @{post.userId}
+                  </span>
+                </div>
+                <div className='post__bodySp' >
+                  <p >{post.body}</p>
+                </div>
+                <div className='post__image' >
+                  <Photos postId={post.id} />
+                </div>
+              </div>
+              <div >
+                <div className='post__dropdown'>
+                  <span className='post__span'>...</span>
+                  <div className='post__dropdownSp'>
+                    {/* აქ უნდა გავხსნა dropDown  */}
+                    <div className='post__dropdownContent'>
+                      <p>like</p>
+                      <p>See Tweet</p>
+                    </div>
+                    {/*  */}
+                  </div>
+                </div>
+                <div className='post__icons'>
+                  <ChatBubbleOutline />
+                  <Repeat />
+                  <Favorite />
+                  <BarChart />
+                  <BookmarkBorder />
+                </div>
+                <Comments />
               </div>
             </div>
-            <div className='post__icons'>
-              <ChatBubbleOutline />
-              <Repeat />
-              <Favorite />
-              <BarChart />
-              <BookmarkBorder />
-            </div>
-            <Comments />
           </div>
-        </div>
-      </div>
-    ))}
-    </div>
+        ))}
+        </>
+      )}
+    </>
   )
 }
 
